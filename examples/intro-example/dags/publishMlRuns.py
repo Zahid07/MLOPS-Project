0b0fcf16679705eada4277eb2160816150ead651
Zahid07 <zahidmuhammad127@gmail.com>
2023-06-08 17:55:41 +0500

    changed publishMlRuns

--- examples/intro-example/dags/publishMlRuns.py
+++ examples/intro-example/dags/publishMlRuns.py
@@ -1,12 +1,20 @@
 import os
 
 def pushToGit():
-    # print the folders in current directory
-    print(os.listdir())
-    # push folder mlruns to github branch mlFlow from the current directory
+    # check if MLFlow-Project exists
+    if os.path.exists("MLOPS-Project"):
+        # remove the MLFlow-Project folder
+        os.system("rm -rf MLOPS-Project")
+
+    # clone the repo again
+    os.system("git clone -b mlFlow --single-branch https://github.com/Zahid07/MLOPS-Project.git")	
+    # add fodler mlruns to git
     os.system("git add mlruns")
-    os.system("git commit -m 'mlruns added'")
+    # commit the changes
+    os.system("git commit -m \"Added mlruns folder\"")
+    # push the changes to the remote repo
     os.system("git push origin mlFlow")
-    print("mlruns pushed to github successfully.")
-    
+    print("Pushed mlruns folder to remote repo successfully.")
+
+
     
