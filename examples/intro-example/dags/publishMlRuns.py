0d5b2cac038f45c8b1e9c2a3c913d0872d8f75ea
Zahid07 <zahidmuhammad127@gmail.com>
2023-06-08 18:02:24 +0500

    Added Git Commit

--- examples/intro-example/dags/publishMlRuns.py
+++ examples/intro-example/dags/publishMlRuns.py
@@ -6,6 +6,11 @@ def pushToGit():
         # remove the MLFlow-Project folder
         os.system("rm -rf MLOPS-Project")
 
+    # add username and email to git
+    os.system("git config --global user.email 'zahidmuhammad127@gmail.com'")
+    os.system("git config --global user.name 'Zahid07'")
+    
+
     # clone the repo again
     os.system("git clone -b mlFlow --single-branch https://github.com/Zahid07/MLOPS-Project.git")	
     # copy and replace the mlruns folder into the MLFlow-Project folder
@@ -19,7 +24,7 @@ def pushToGit():
     # push the changes
     os.system("git push origin mlFlow")
     print("Pushed to git successfully.")
-    
+
 
 
 
