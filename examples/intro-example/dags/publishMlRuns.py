f5b7a7c00f3c46b2dbb9f898ab400ca5158482d4
Zahid07 <zahidmuhammad127@gmail.com>
2023-06-08 17:48:04 +0500

    Changed publish file

--- examples/intro-example/dags/publishMlRuns.py
+++ examples/intro-example/dags/publishMlRuns.py
@@ -3,4 +3,10 @@ import os
 def pushToGit():
     # print the folders in current directory
     print(os.listdir())
+    # push folder mlruns to github branch mlFlow from the current directory
+    os.system("git add mlruns")
+    os.system("git commit -m 'mlruns added'")
+    os.system("git push origin mlFlow")
+    print("mlruns pushed to github successfully.")
+    
     
